8199eadfcec84b9823a3ce026607347d0043b25f
Logan <logan@brighthive.io>
2020-05-11 12:50:28 -0700

    WIP: RED

--- tests/api_manager/test_api_manager.py
+++ tests/api_manager/test_api_manager.py
@@ -14,20 +14,20 @@ def test_end_to_end(VALID_DATA_DICTIONARY, e2e_app):
     print(body)
     assert len(body) == 0
 
-    # # POST
-    # response = api.post("/people", json={"name": "testname"})
-    # assert response.status_code == 201
+    # POST
+    response = api.post("/peoples", json={"name": "testname"})
+    assert response.status_code == 200
 
-    # body = json.loads(response.data)
-    # print(body)
+    body = json.loads(response.data)
+    print(body)
 
-    # # CHECK THAT POST WORKED
-    # response = api.get("/people", json={})
-    # assert response.status_code == 200
+    # CHECK THAT POST WORKED
+    response = api.get("/peoples", json={})
+    assert response.status_code == 200
 
-    # body = json.loads(response.data)
-    # print(body)
-    # assert len(body) == 1
+    body = json.loads(response.data)
+    print(body)
+    assert len(body) == 1
 
 
 # def test_get_response(VALID_DATA_DICTIONARY, api):

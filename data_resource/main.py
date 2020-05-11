437f3e9ca044331b80fea18b9f6a267033cf93e2
Logan <logan@brighthive.io>
2020-05-11 12:01:27 -0700

    WIP: GREEN

--- data_resource/main.py
+++ data_resource/main.py
@@ -1,14 +1,23 @@
 import connexion
 from connexion.resolver import Resolver
+from connexion import NoContent
+
+
+def resolver_stub(fn_name):
+    print(fn_name)
+
+    def get_stub():
+        return {}, 200
+
+    return get_stub
 
 
 def run(api_dict: dict, actually_run=True):
     app = connexion.FlaskApp(__name__)
 
     _resolver = Resolver()
-    _resolver.function_resolver = lambda x: lambda x: x
+    _resolver.function_resolver = resolver_stub
 
-    # app.add_api('openapi.yaml', resolver=_resolver)
     app.add_api(api_dict, resolver=_resolver)
     application = app.app
 

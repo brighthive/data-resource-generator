0d4e57b8c22d9f5e085c56b81f1b75765f70e2a6
Logan <logan@brighthive.io>
2020-06-02 11:46:33 -0700

    RED: Add unit test case to get all.

--- tests/api_manager/test_rest_functions.py
+++ tests/api_manager/test_rest_functions.py
@@ -6,7 +6,6 @@ from collections import OrderedDict
 def test_get_all(empty_database, valid_people_orm):
     # When nothing in DB, returns nothing
     resource_handler = ResourceHandler()
-
     resource_name = "test"
     resource_orm = valid_people_orm
 
@@ -14,7 +13,15 @@ def test_get_all(empty_database, valid_people_orm):
         name=resource_name, resource_orm=resource_orm, offset=0, limit=10
     )
 
-    assert result == (OrderedDict([("test", []), ("links", [])]), 200)
+    assert len(result[0]["test"]) == 0
 
     # When one item in DB, returns an item
-    # db_session.query(valid_people_orm)
+    new_object = resource_orm(name="tester")
+    db_session.add(new_object)
+    db_session.commit()
+
+    result = resource_handler.get_all(
+        name=resource_name, resource_orm=resource_orm, offset=0, limit=10
+    )
+
+    assert result[0]["test"] == [{"id": 1, "name": "tester"}]

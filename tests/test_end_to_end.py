44e0cce205fead75839bc3bb5cae77446a6eb1ea
Logan <logan@brighthive.io>
2020-06-04 17:01:49 -0700

    chore: E2E test asserts on entire response body.

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -2,7 +2,6 @@ import pytest
 import json
 
 
-# TODO these asserts should assert on the entire response
 @pytest.mark.requiresdb
 def test_end_to_end(
     generated_e2e, empty_database
@@ -11,45 +10,56 @@ def test_end_to_end(
 
     # Check for no data in DB
     response = api.get("/people", json={})
-    assert response.status_code == 200
-
     body = json.loads(response.data)
-    assert len(body["people"]) == 0
+
+    assert response.status_code == 200
+    assert body == {"links": [], "people": []}
 
     # Create a person
     response = api.post("/people", json={"name": "testname"})
+    body = json.loads(response.data)
+
     assert response.status_code == 201
+    assert body == {"message": "Successfully added new resource.", "id": 1}
 
     # Check all the data in the DB
     response = api.get("/people", json={})
-    assert response.status_code == 200
-
     body = json.loads(response.data)
-    assert body["people"] == [{"id": 1, "name": "testname"}]
+
+    assert response.status_code == 200
+    assert body == {
+        "people": [{"id": 1, "name": "testname"}],
+        "links": [
+            {"rel": "self", "href": "/people?offset=0&limit=20"},
+            {"rel": "first", "href": "/people?offset=0&limit=20"},
+            {"rel": "last", "href": "/people?offset=0&limit=20"},
+        ],
+    }
 
     # Check the specific data for the person we created
     response = api.get("/people/1", json={})
-    assert response.status_code == 200
-
     body = json.loads(response.data)
 
-    assert body["id"] == 1
-    assert body["name"] == "testname"
+    assert response.status_code == 200
+    assert body == {"name": "testname", "id": 1}
 
     # DELETE is not implemented. For reasons!
     response = api.delete("/people/1", json={})
+    body = json.loads(response.data)
+
     assert response.status_code == 405
+    assert body == {"message": "Unimplemented unsecure delete"}
 
     # Modify the item
     response = api.put("/people/1", json={"name": "newname"})
-    assert response.status_code == 201
-
     body = json.loads(response.data)
-    assert body["id"] == 1
+
+    assert response.status_code == 201
+    assert body == {"message": "Successfully updated resource.", "id": 1}
 
     # Patch the item
     response = api.patch("/people/1", json={"name": "patched"})
-    assert response.status_code == 201
-
     body = json.loads(response.data)
-    assert body["id"] == 1
+
+    assert response.status_code == 201
+    assert body == {"message": "Successfully updated resource.", "id": 1}

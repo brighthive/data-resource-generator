2106cd1da6bc1e826244068076d124879dd08362
Logan <logan@brighthive.io>
2020-06-10 10:48:01 -0700

    Green: feat: Add query.

--- tests/api_manager/rest_functions/test_single_post.py
+++ tests/api_manager/rest_functions/test_single_post.py
@@ -1,7 +1,6 @@
 from data_resource.generator.api_manager.v1_0_0.resource_handler import ResourceHandler
 import pytest
 from data_resource.logging.api_exceptions import ApiError
-from data_resource.db.base import db_session
 
 
 @pytest.mark.requiresdb
@@ -21,6 +20,7 @@ def test_post(empty_database, valid_people_orm):
     assert result[1] == 201
 
 
+@pytest.mark.requiresdb
 def test_post_missing_required_fields(empty_database, valid_orm_with_required_field):
     # Creating without required fields errors
     resource_handler = ResourceHandler()
@@ -33,57 +33,3 @@ def test_post_missing_required_fields(empty_database, valid_orm_with_required_fi
         _ = resource_handler.insert_one(
             resource_orm=resource_orm, request=FakeFlaskRequest()
         )
-
-
-def test_query_works_with_correct_data(empty_database, valid_people_orm):
-    resource_handler = ResourceHandler()
-    resource_orm = valid_people_orm
-
-    class FakeFlaskRequest:
-        json = {"name": "tester"}
-
-    new_object = resource_orm(name="tester")
-    db_session.add(new_object)
-    db_session.commit()
-
-    result = resource_handler.query_one(
-        resource_orm=resource_orm, request=FakeFlaskRequest()
-    )
-
-    assert result == ({"results": [{"id": 1, "name": "tester"}]}, 200)
-
-
-def test_query_errors_when_given_field_that_does_not_exist(
-    empty_database, valid_people_orm
-):
-    # When one item in DB - GET returns that item
-    resource_handler = ResourceHandler()
-    resource_orm = valid_people_orm
-
-    class FakeFlaskRequest:
-        json = {"name": "wrongname"}
-
-    new_object = resource_orm(name="tester")
-    db_session.add(new_object)
-    db_session.commit()
-
-    result = resource_handler.query_one(
-        resource_orm=resource_orm, request=FakeFlaskRequest()
-    )
-
-    assert result == ({"message": "No matches found"}, 404)
-
-
-def test_query_empty_body_errors(empty_database, valid_people_orm):
-    resource_handler = ResourceHandler()
-    resource_orm = valid_people_orm
-
-    class FakeFlaskRequest:
-        json = {}
-
-    with pytest.raises(ApiError):
-        resource_handler.query_one(
-            resource_orm=resource_orm, request=FakeFlaskRequest()
-        )
-
-    # assert result == ({"error": "No fields found in body."}, 400)
---
+++ tests/api_manager/rest_functions/test_single_query.py
@@ -0,0 +1,82 @@
+from data_resource.generator.api_manager.v1_0_0.resource_handler import ResourceHandler
+import pytest
+from data_resource.logging.api_exceptions import ApiError
+from data_resource.db.base import db_session
+
+
+@pytest.mark.requiresdb
+def test_query_works_with_correct_data(empty_database, valid_people_orm):
+    resource_handler = ResourceHandler()
+    resource_orm = valid_people_orm
+
+    class FakeFlaskRequest:
+        json = {"name": "tester"}
+
+    new_object = resource_orm(name="tester")
+    db_session.add(new_object)
+    db_session.commit()
+
+    result = resource_handler.query_one(
+        resource_orm=resource_orm, request=FakeFlaskRequest()
+    )
+
+    assert result == ({"results": [{"id": 1, "name": "tester"}]}, 200)
+
+
+@pytest.mark.requiresdb
+def test_query_returns_none_when_given_incorrect_field_data(
+    empty_database, valid_people_orm
+):
+    # When one item in DB - GET returns that item
+    resource_handler = ResourceHandler()
+    resource_orm = valid_people_orm
+
+    class FakeFlaskRequest:
+        json = {"name": "wrongname"}
+
+    new_object = resource_orm(name="tester")
+    db_session.add(new_object)
+    db_session.commit()
+
+    result = resource_handler.query_one(
+        resource_orm=resource_orm, request=FakeFlaskRequest()
+    )
+
+    assert result == ({"message": "No matches found"}, 404)
+
+
+@pytest.mark.requiresdb
+def test_query_empty_body_errors(empty_database, valid_people_orm):
+    resource_handler = ResourceHandler()
+    resource_orm = valid_people_orm
+
+    class FakeFlaskRequest:
+        json = {}
+
+    with pytest.raises(ApiError):
+        resource_handler.query_one(
+            resource_orm=resource_orm, request=FakeFlaskRequest()
+        )
+
+    # assert result == ({"error": "No fields found in body."}, 400)
+
+
+@pytest.mark.requiresdb
+def test_query_errors_when_given_incorrect_field_data(empty_database, valid_people_orm):
+    # When one item in DB - GET returns that item
+    resource_handler = ResourceHandler()
+    resource_orm = valid_people_orm
+
+    class FakeFlaskRequest:
+        json = {"doesnotexist": "error"}
+
+    new_object = resource_orm(name="tester")
+    db_session.add(new_object)
+    db_session.commit()
+
+    with pytest.raises(ApiError):
+        resource_handler.query_one(
+            resource_orm=resource_orm, request=FakeFlaskRequest()
+        )
+
+    # assert result == ({"errors": "Unknown or restricted field '{}' found."}, 400)

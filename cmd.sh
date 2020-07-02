a62d13bc8397db7e992504c8d9b1c7f509bdb5fa
John O'Sullivan <jnosullivan@icloud.com>
2020-07-02 11:10:46 -0400

    Clean up

--- cmd.sh
+++ cmd.sh
@@ -4,6 +4,5 @@ if [ "$APP_ENV" == "DEVELOPMENT" ] || [ -z "$APP_ENV" ]; then
     gunicorn -w 4 -b 0.0.0.0:8081 wsgi:app --reload --worker-class gevent --timeout 600  # nosec
 else
   sleep 10
-  # pipenv run python wsgi.py
-  gunicorn -b 0.0.0.0:8081 -w 4 wsgi:app --worker-class gevent
+  gunicorn -b 0.0.0.0:8081 -w 1 wsgi:app --worker-class gevent
 fi

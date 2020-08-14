9428fbabed687568a6d5f8d7cde9317cd3893e7c
Logan <logan@brighthive.io>
2020-08-13 17:21:51 -0700

    chore: Clean up resource_create

--- data_resource/generator/api_manager/v1_0_0/crud_functions/resource_create.py
+++ data_resource/generator/api_manager/v1_0_0/crud_functions/resource_create.py
@@ -23,34 +23,11 @@ class ResourceCreate:
         except Exception:
             raise ApiError("No request body found.", 400)
 
-        # Validate our schema? This should have already occured # TODO add test for do not let unvalidated tableschema in
-        # _ = Schema(table_schema)
-        # errors = []
-        # accepted_fields = []
-
-        # if not validate(table_schema):
-        # raise SchemaValidationFailure()
-        # raise
-
-        # Check for required fields # Need tableschema to find required items... won't this be in the ORM? TODO check ORM for required? try except?
-        # for field in table_schema["fields"]:
-        # accepted_fields.append(field["name"])
-
-        # if field["required"] and not field["name"] in request_obj.keys():
-        # errors.append(f"Required field '{field['name']}' is missing.")
-
-        # valid_fields = []
-
-        # check if the value is in our tbl schema or assume its a many to many
-        # removed code
-
-        # TODO is it a security concern to destructure anything into sqlalchemy model?
         try:
             new_object = resource_orm()
             for key, value in request_obj.items():
                 setattr(new_object, key, value)
 
-            # try except here to catch errors? # TODO
             db_session.add(new_object)
             db_session.commit()
 
